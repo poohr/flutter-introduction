33c7a1e95786230b3a31226ebc3317b23a92c8d5
naoki <naoki@qict.co.jp>
2024-06-13 06:18:46 +0900

    [update] セクション3:ウィジェット 49.ウィジェットの紹介その9（Aligmentウィジェット）

--- lib/main.dart
+++ lib/main.dart
@@ -38,17 +38,10 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          const ColoredBox(
-              color: Colors.red,
-              child: Text('こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！')),
-          const ColoredBox(
-              color: Color.fromARGB(255, 255, 0, 0),
-              child: Text('こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！こんにちわ！')),
-
-        ],
-      ),
+      body: Align(
+        alignment: Alignment.bottomLeft,
+        child: Text('おはよう！おはよう！おはよう！おはよう！'),
+      )
     );
   }
 }
